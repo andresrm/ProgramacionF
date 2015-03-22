094934935663b6c8907b97a044f009dc74913f59
Andres Rodriguez <airgzm@gmail.com>
2015-03-21 20:15:58 -0700

    Agregando Producto1

---
+++ Producto2/juego.py
@@ -0,0 +1,28 @@
+import time
+
+print "Hola! Tratare de adivinar un numero."
+import time
+time.sleep(5)
+
+print "Piensa un numero entre 1 y 10."
+import time
+time.sleep(5)
+
+print "Multiplicalo por 9"
+import time
+time.sleep(5)
+
+print "Si el numero es de dos digitos, sumalos entre si: Ej. 36  -> 3+6=9. Si tu numero tiene un solo digito, sumale 0"
+import time
+time.sleep(5)
+
+print "Al numero resultante sumale 4"
+import time
+time.sleep(10)
+
+print "Muy bien. El resultado es 13 :)"
+print ""
+import time
+time.sleep(5)
+import time
+time.sleep(5)
